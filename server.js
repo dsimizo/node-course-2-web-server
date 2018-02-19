086d397a8bdb68a21908b5f0ed63592d825f964f
dsimizo <dsimizo@gmail.com>
2018-02-18 21:50:42 -0300

    Make equal to example

--- server.js
+++ server.js
@@ -59,8 +59,7 @@ app.get('/bad', (req, res) => {
 
 app.get('/projects', (req, res)=> {
   res.render('projects.hbs', {
-    pageTitle: 'Projects Page',
-    projectsMessage: 'My projects here'
+    pageTitle: 'Projects'
   });
 });
 
